a3a5fe2c2b3ba80573bfe8a733483bcd16751ba8
Steve Klabnik <steve@steveklabnik.com>
2019-09-25 16:54:41 -0500

    Add some redirects

--- workers-site/index.js
+++ workers-site/index.js
@@ -5,6 +5,16 @@ addEventListener("fetch", event => {
 });
 
 async function handleEvent(event) {
+
+  // redirect some old URLs
+  let pathname = new URL(event.request.url).pathname;
+
+  if (pathname === "/security.html") {
+    return Response.redirect("https://www.steveklabnik.com/security/", 301);
+  } else if (pathname === "/deleuzional.html") {
+    return Response.redirect("https://www.steveklabnik.com/deleuzional/", 301);
+  }
+
   try {
     return await getAssetFromKV(event);
   } catch (e) {
